86454609557648f3985ec4b12fc6120b6b9df9b4
Ayala-k <kluftayala@gmail.com>
2023-11-29 09:53:22 +0200

    a

--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -5,6 +5,6 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.7nbv7ig.mongodb.net/my_db_basmach`);
-  console.log("mongo connect")
+  console.log("mongo connectedddd")
 }
 
